e6106caadeed367abdba10b80377b49ef49293a5
Shivam Vishwakarma <arvind.972176@gmail.com>
2022-10-25 19:43:53 +0530

    25-10-2022

---
+++ Pepcoding Web Development/2.Intro to JavaScript/15. Promises/promiseExample.js
@@ -0,0 +1,21 @@
+let fs = require("fs");
+// async code 
+console.log("Before");
+// fs.readFile("f1.txt", function (err, data) {
+//     console.log(data);
+// });
+
+// Initially promise is in pending state
+let promise = fs.promises.readFile("f11.txt");
+console.log(promise);
+// settled
+// fullfilled
+promise.then(function (data) {
+    console.log("" + data);
+})
+// rejected
+promise.catch(function (err) {
+    console.log(err);
+})
+// promise
+console.log("after");
