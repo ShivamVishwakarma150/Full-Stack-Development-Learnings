1d6fe81269bf9dcae7b84f65b7d512c2a0885092
Shivam Vishwakarma <arvind.972176@gmail.com>
2022-12-07 12:31:36 +0530

    07-12-2022

--- Pepcoding Web Development/7. Browser Module/24. JIRA Project - 1/script.js
+++ Pepcoding Web Development/7. Browser Module/24. JIRA Project - 1/script.js
@@ -2,8 +2,26 @@
 let addBtn = document.querySelector(".add-btn");
 let modalCont = document.querySelector(".modal-cont");
 let textAreaCont = document.querySelector(".textarea-cont");
-let addFlag = false;
+
+let allPriorityColor = document.querySelectorAll(".priority-color");
+
 let mainCont = document.querySelector(".main-cont");
+let colors = ["lightpink","lightblue","lightgreen","black"];
+let modalPriorityColor = colors[colors.length-1];
+
+let addFlag = false;
+
+// Listener For Modal Priority Coloring
+allPriorityColor.forEach((colorElem,idx)=>{
+    colorElem.addEventListener("click",(e)=>{
+        allPriorityColor.forEach((priorityColorElem,idx)=>{
+            priorityColorElem.classList.remove("border");
+        })
+        colorElem.classList.add("border");
+        modalPriorityColor = colorElem.classList[0];
+
+    })
+})
 
 addBtn.addEventListener("click",(e)=>{
     // Display Modal
@@ -27,20 +45,20 @@ addBtn.addEventListener("click",(e)=>{
 modalCont.addEventListener("keydown",(e)=>{
     let key=e.key;
     if(key === "Shift"){
-        createTicket();
+        createTicket(modalPriorityColor,textAreaCont.value,shortid());
         modalCont.style.display="none";
         addFlag = false;
         textAreaCont.value="";
     }
 })
 
-function createTicket(){
+function createTicket(ticketColor,ticketTask,ticketID){
     let ticketCont = document.createElement("div");
     ticketCont.setAttribute("class","ticket-cont");
-    ticketCont.innerHTML = `<div class="ticket-cont">
-    <div class="ticket-color"></div>
-    <div class="ticket-id">Sample id</div>
-    <div class="task-area">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem velit eaque minima eum fugit voluptatibus.</div>
+    ticketCont.innerHTML = `
+    <div class="ticket-color ${ticketColor}"></div>
+    <div class="ticket-id ">${ticketID}</div>
+    <div class="task-area ">${ticketTask}</div>
 
 </div>`;
     mainCont.appendChild(ticketCont);
